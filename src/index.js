3424c2e1a832bd71fe99bbbb74cd5c102deb0bcf
Vladimir <44143087+VladimirZverugo@users.noreply.github.com>
2021-01-23 15:43:59 +0300

    init: upload task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    let arr = [] ;
+    let temp2 = [];
+    let temp4= [];
+    if (matrix && matrix.length){
+
+        if (matrix.length === 2) {
+            temp2 = matrix[1].reverse();
+            arr = arr.concat(matrix[0]).concat(temp2);
+        }else if (matrix.length === 3) {
+            temp2 = matrix[1].reverse();
+            arr = arr.concat(matrix[0]).concat(temp2).concat(matrix[2]);
+        }else if (matrix.length === 4) {
+            temp2 = matrix[1].reverse();
+            temp4 = matrix[3].reverse();
+            arr = arr.concat(matrix[0]).concat(temp2).concat(matrix[2]).concat(temp4);
+        }
+        return arr
+
+    }else{
+            return [] ;
+        }
 }
+
